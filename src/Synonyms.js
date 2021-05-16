f9e757c705ccddf7b57d5f360047276d2f1734f2
coder-derie <77130913+coder-derie@users.noreply.github.com>
2021-05-16 09:58:30 -0400

    Updated Synonyms

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -5,9 +5,7 @@ export default function Synonyms(props){
        return(
            <ul className="Synonyms">
                {props.synonyms.map(function (synonym, index){
-                   <li key={index}>
-                       {synonym}
-                   </li>
+                   return <li key={index}>{synonym}</li>
                })}
            </ul>
        ) 
