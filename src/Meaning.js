b737cfdde53c7b59998559b034959de65cf9c3c0
coder-derie <77130913+coder-derie@users.noreply.github.com>
2021-05-16 13:56:12 -0400

    Rendered the Phonetics component

--- src/Meaning.js
+++ src/Meaning.js
@@ -9,13 +9,10 @@ export default function Meaning(props){
             {props.meanings.definitions.map(function (definition, index){
                 return (
                     <div key={index}>
-                       <p>{definition.definition}
-                       <br/>
-                       <em>{definition.example}</em>
+                       <div className="definition">{definition.definition}</div>
+                       <div className="example">{definition.example}</div>
                        <Synonyms synonyms={definition.synonyms} />
-                    </p>
-                       
-                       </div>
+                    </div>
                 )
             })}
         </div>
