826e2c9c458429679399fd02ee16a44b86eaf4c0
coder-derie <77130913+coder-derie@users.noreply.github.com>
2021-05-16 22:36:59 -0400

    Rendered Photos component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,24 +1,32 @@
 import React, { useState }from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props){
     let [word, setWord]= useState(props.defaultWord);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
-    function handleResponse(response){
+    function handleDictionaryResponse(response){
         console.log(response.data[0]);
         setResults(response.data[0]);
+    }
+
+    function handlePexelsResponse(response){
+        setPhotos(response.data.photos);
     }
 
     function search(){
          //api documentation can be found on Google Dictionary Api
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${word}`;
-        axios.get(apiUrl).then(handleResponse);
-        console.log(apiUrl);
-    
+        axios.get(apiUrl).then(handleDictionaryResponse);
+        
+        let pexelsApiKey = "563492ad6f9170000100000168c545eb1716465585056ea9127bb4bc";
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${word}&per_page=9`;
+        axios.get(pexelsApiUrl, { headers: {"Authorization" : `Bearer ${pexelsApiKey}`} }).then(handlePexelsResponse);
     }
     
     function handleSubmit(event){
@@ -46,6 +54,7 @@ export default function Dictionary(props){
             </form>
             </section>
             <Results results={results}/>
+            <Photos photos={photos} />
         </div>
     )
     } else {
