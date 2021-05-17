a009eab47e5b6d72db9cb8531f63841a1ad4b085
coder-derie <77130913+coder-derie@users.noreply.github.com>
2021-05-16 22:40:05 -0400

    Reformatted coding structure

--- src/Photos.js
+++ src/Photos.js
@@ -13,7 +13,7 @@ export default function Photos(props){
                         <a href={photo.src.original} target="_blank" rel="noreferrer">
                         <img src={photo.src.landscape} className="img-fluid"  alt={photo.src.photographer}/>
                         </a>
-                    </div>
+                        </div>
                     ) 
                     
                 })}
